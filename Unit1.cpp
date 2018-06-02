b55292d2ce58061ff717c735d6507ec38ec40ba4
JAKKRA06 <jakub.krajniewski.programista@gmail.com>
2018-06-02 12:51:38 +0200

    Bug fixed

--- Unit1.cpp
+++ Unit1.cpp
@@ -8,10 +8,24 @@
 #pragma package(smart_init)
 #pragma resource "*.dfm"
 TForm1 *Form1;
+
+int x = -8;
+int y = -8;
 //---------------------------------------------------------------------------
 __fastcall TForm1::TForm1(TComponent* Owner)
         : TForm(Owner)
 {
 }
 //---------------------------------------------------------------------------
- 
+
+void __fastcall TForm1::Timer_ballTimer(TObject *Sender)
+{
+  ball->Left += x;
+  ball->Top += y;
+
+  //odbicie od top
+  if(ball->Top <= 0) y = -y;
+  //odbicie od bottom
+  if(ball->Top + ball->Height >= tlo->Height) y = -y;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+}
+//---------------------------------------------------------------------------
