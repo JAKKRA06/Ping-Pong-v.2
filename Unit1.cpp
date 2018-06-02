1510ca5a9cce77cd507b74620f3f3d73cf0a4c73
JAKKRA06 <jakub.krajniewski.programista@gmail.com>
2018-06-02 15:07:23 +0200

    paddles control

--- Unit1.cpp
+++ Unit1.cpp
@@ -7,18 +7,20 @@
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
 #pragma resource "*.dfm"
-TForm1 *Form1;
+TPingPong *PingPong;
 
 int x = -8;
 int y = -8;
+int scorePlayerLeft = 0;
+int scorePlayerRight = 0;
 //---------------------------------------------------------------------------
-__fastcall TForm1::TForm1(TComponent* Owner)
+__fastcall TPingPong::TPingPong(TComponent* Owner)
         : TForm(Owner)
 {
 }
 //---------------------------------------------------------------------------
 
-void __fastcall TForm1::Timer_ballTimer(TObject *Sender)
+void __fastcall TPingPong::Timer_ballTimer(TObject *Sender)
 {
   ball->Left += x;
   ball->Top += y;
@@ -26,6 +28,53 @@ void __fastcall TForm1::Timer_ballTimer(TObject *Sender)
   //odbicie od top
   if(ball->Top <= 0) y = -y;
   //odbicie od bottom
-  if(ball->Top + ball->Height >= tlo->Height) y = -y;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+  if(ball->Top + ball->Height >= tlo->Height) y = -y;
+  //skucha
+  if(ball->Left <= paddleLeft->Left + paddleLeft->Width)
+  {
+     Timer_ball->Enabled = false;
+     ball->Visible = false;
+     scorePlayerRight++;
+  }
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+}
+//---------------------------------------------------------------------------
+void __fastcall TPingPong::LeftUpTimer(TObject *Sender)
+{
+  if(paddleLeft->Top >= 10) paddleLeft->Top -= 10;
+}
+//---------------------------------------------------------------------------
+void __fastcall TPingPong::LeftDownTimer(TObject *Sender)
+{
+  if(paddleLeft->Top + paddleLeft->Height <= tlo->Height+10) paddleLeft->Top += 10;
+}
+//---------------------------------------------------------------------------
+void __fastcall TPingPong::FormKeyDown(TObject *Sender, WORD &Key,
+      TShiftState Shift)
+{
+   if(Key == 0x51) LeftUp->Enabled = true;
+   if(Key == 0x41) LeftDown->Enabled = true;
+   if(Key == VK_UP) RightUp->Enabled = true;
+   if(Key == VK_DOWN) RightDown->Enabled = true;
+}
+//---------------------------------------------------------------------------
+void __fastcall TPingPong::FormKeyUp(TObject *Sender, WORD &Key,
+      TShiftState Shift)
+{
+   if(Key == 0x51) LeftUp->Enabled = false;
+   if(Key == 0x41) LeftDown->Enabled = false;
+   if(Key == VK_UP) RightUp->Enabled = false;
+   if(Key == VK_DOWN) RightDown->Enabled = false;
+}
+//---------------------------------------------------------------------------
+void __fastcall TPingPong::RightUpTimer(TObject *Sender)
+{
+  if(paddleRight->Top >= 10) paddleRight->Top -= 10;
+}
+//---------------------------------------------------------------------------
+void __fastcall TPingPong::RightDownTimer(TObject *Sender)
+{
+  if(paddleRight->Top + paddleRight->Height + 10 <= tlo->Height) paddleRight->Top += 10;
 }
 //---------------------------------------------------------------------------
--- Unit1.h
+++ Unit1.h
@@ -10,20 +10,33 @@
 #include <ExtCtrls.hpp>
 #include <Graphics.hpp>
 //---------------------------------------------------------------------------
-class TForm1 : public TForm
+class TPingPong : public TForm
 {
 __published:	// IDE-managed Components
         TShape *tlo;
         TImage *paddleLeft;
         TImage *ball;
         TTimer *Timer_ball;
+        TTimer *LeftUp;
+        TTimer *LeftDown;
+        TImage *paddleRight;
+        TTimer *RightUp;
+        TTimer *RightDown;
         void __fastcall Timer_ballTimer(TObject *Sender);
+        void __fastcall LeftUpTimer(TObject *Sender);
+        void __fastcall LeftDownTimer(TObject *Sender);
+        void __fastcall FormKeyDown(TObject *Sender, WORD &Key,
+          TShiftState Shift);
+        void __fastcall FormKeyUp(TObject *Sender, WORD &Key,
+          TShiftState Shift);
+        void __fastcall RightUpTimer(TObject *Sender);
+        void __fastcall RightDownTimer(TObject *Sender);
 private:	// User declarations
 public:		// User declarations
-        __fastcall TForm1(TComponent* Owner);
+        __fastcall TPingPong(TComponent* Owner);
 };
 //---------------------------------------------------------------------------
-extern PACKAGE TForm1 *Form1;
+extern PACKAGE TPingPong *PingPong;
 //---------------------------------------------------------------------------
 #endif
  
